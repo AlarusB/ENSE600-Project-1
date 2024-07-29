e9084656eb77b0447174028bfe5b646a06b64401
alexs <alexs@DESKTOP-NLHTUBO>
2024-07-29 17:57:45 +1200

    ><>

---
+++ ENSE600_Project_1/src/main/java/Player.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alexs
+ */
+public class Player {
+
+    private String name;
+    private int health;
+    private int attack;
+
+    public Player(String name, int health, int attack) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+}
