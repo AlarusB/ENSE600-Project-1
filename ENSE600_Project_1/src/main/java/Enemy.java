4545ae1d8384eba6f8d16336ea28409c1879b0fc
alexs <alexs@DESKTOP-NLHTUBO>
2024-08-26 15:53:58 +1200

    stuff

--- ENSE600_Project_1/src/main/java/Enemy.java
+++ ENSE600_Project_1/src/main/java/Enemy.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,26 @@
  *
  * @author alexs
  */
-public class Enemy {
-    
+public class Enemy implements Serializable {
+    private String name;
+    private int level;
+    private int baseHP;
+    private double levelMultiplier;
+
+    public Enemy(String name, int level, int baseHP, double levelMultiplier, int challengeRating) {
+        this.name = name;
+        this.level = level;
+        this.baseHP = baseHP;
+        this.levelMultiplier = levelMultiplier;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int calculateMaxHP() {
+        return (int) (baseHP * levelMultiplier);
+    }
+
+
 }
